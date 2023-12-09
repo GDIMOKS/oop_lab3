6c94a50b3b431d135436b1a363611c94f9638f1f
GDIMOKS <57040046+GDIMOKS@users.noreply.github.com>
2023-12-09 18:55:20 +0300

    Initial commit

---
+++ DataAccess/ShopDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class ShopDbContext : DbContext
+{
+    public ShopDbContext()
+    {
+        
+    }
+
+    public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+    {
+        
+    }
+    // DbSets
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //подключение к бд
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //настройки
+    }
+}
